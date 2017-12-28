7b96a2090c9fed5b1efe242e998495c95eb086d9
Perri <perinay.adam@gmail.com>
2017-12-28 23:17:53 +0100

    DB Data update

--- Config.php
+++ Config.php
@@ -5,7 +5,7 @@ return array
     'title' => "GeoExkurzie",
     'backend_version' => '0.0.1',
     'db_host' => "localhost",
-    'db_user' => "root",
-    'db_pass' => "",
-    'db_name' => "inf"
+    'db_user' => "perinay.a",
+    'db_pass' => "porovnanie",
+    'db_name' => "perinay.a"
 );
